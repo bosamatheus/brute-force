ef4d2707da516c805263d8af8d026c6f11c344dd
Matheus Felipe Bosa <matheusbozza@gmail.com>
2021-01-10 18:14:52 -0300

    Medium solution

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # Brute Force
 
+## Challenge
+
 The challenge contains 4 levels of difficult:
 - Easy: Generate all combinations of numbers with 4 digits [0, 9999];
 - Medium: Generate all combinations of number and letters, uppercase and lowercase, until 8 characters;
-- Advanced: Input a ASCII characters list and k parameter that means the maximum lenght for the password, generating all possible combinations;
+- Advanced: Input a ASCII characters list and k parameter that means the maximum length for the password, generating all possible combinations;
 - Expert: Resolve the Advanced level using t parallel threads.
 
 Let's simulate this using password protected .zip!
+
+## Key Learnings
+
+If you have chosen Python to solve this problem, you will be able to learn about:
+- Generators and yield keyword;
+- Parallelism, ThreadPoolExecutor and ProcessPoolExecutor.
+
+## Execution
+
+To run the program use:
+
+```python
+python main.py -z files/easy.zip
+```
--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import argparse
+from time import time
 from zipfile import ZipFile, BadZipFile
+import string
+import itertools
 
 parser = argparse.ArgumentParser(description="Unzips a password protected .zip", usage="main.py -z easy.zip")
 parser.add_argument("-z", "--zip", metavar="", required=True, help="Location and the name of the .zip file.")
 args = parser.parse_args()
 
-RANGE_PASSWORD = 10000
-
 def main(zip_file):
+    print("Running brute force...")
     with ZipFile(zip_file) as zf:
-        zf.printdir()
-        for password in range(RANGE_PASSWORD):
-            if extract(zf, password=f"{password:04}"):
-                print(f"File extracted with: {password}")
-                break
+        start = time()
+        password = brute_force(zf, max_length=8)
+        end = time()
+    print(f"Password: {password}")
+    print(f"Total time: {end-start:.3f} seconds")
+
+def brute_force(zf, max_length):
+    for length in range(1, max_length+1):
+        print(f"Trying passwords with {length} character(s)...")
+        for password in password_generator(length):
+            if extract(zf, password):
+                return password
+    return "Not found"
+
+def password_generator(length):
+    chars = string.digits + string.ascii_letters # "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for combination in itertools.product(chars, repeat=length):
+        yield "".join(combination)
 
 def extract(zf, password):
     try:
         zf.extractall(pwd = bytes(password, "utf-8"))
         return True
     except (RuntimeError, BadZipFile) as e:
-        pass
+        return False
 
 if __name__ == "__main__":
     main(args.zip)
